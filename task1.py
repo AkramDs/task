32d828a173b6ba833b952ac3e79913c7e11bc148
Akram Dshakipov <XNKCTEP@gmail.com>
2019-11-06 09:08:16 +0600

    Проверка делимости на двойку

---
+++ task1.py
@@ -0,0 +1,13 @@
+def test_is_even():
+    assert is_even(2) == True
+    assert is_even(3) == False
+    assert is_even(8) == True
+    assert is_even(100) == True
+    assert is_even(101) == False
+    print("YOUR CODE IS CORRECT!")
+
+def is_even(number):
+    if number % 2 == 0 : return True
+    return False
+
+test_is_even()
